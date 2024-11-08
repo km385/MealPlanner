83b0134c26ca361eaeda8e50b1aa776fbb51df79
Jakub Furtak <kuba.furtak.pl@gmail.com>
2024-11-08 15:42:17 +0100

    Initial commit with frontend and backend structure

---
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.backend.entities.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
