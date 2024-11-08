9019df557fdaa2b9e9ce012e218f93ca77bc7a25
Jakub Furtak <kuba.furtak.pl@gmailc.com>
2024-11-08 15:42:17 +0100

    Initial commit with frontend and backend structure

---
+++ backend/src/main/java/com/example/backend/responses/LoginResponse.java
@@ -0,0 +1,27 @@
+package com.example.backend.responses;
+
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public LoginResponse setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    public LoginResponse setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+        return this;
+    }
+
+ // Getters and setters...
+}
