d30f0748b2ee9774649ce1d45cb6685c2c539091
Jakub Furtak <kuba.furtak.pl@gmail.com>
2024-11-09 15:32:32 +0100

    added new tables to the database

--- backend/src/main/java/com/example/backend/entities/Recipe.java
+++ backend/src/main/java/com/example/backend/entities/Recipe.java
@@ -1,13 +1,14 @@
 package com.example.backend.entities;
 
 import java.util.Date;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,15 +16,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "recipes")
+@Getter
+@Setter
 public class Recipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -44,53 +50,56 @@ public class Recipe {
     @JsonIgnore
     private User user;
 
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private Set<RecipeIngredient> recipeIngredients;
+
     // Getters and Setters
 
-    public Integer getId() {
-        return id;
-    }
+    // public Integer getId() {
+    //     return id;
+    // }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    // public void setId(Integer id) {
+    //     this.id = id;
+    // }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
 
-    public String getInstructions() {
-        return instructions;
-    }
+    // public String getInstructions() {
+    //     return instructions;
+    // }
 
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
-    }
+    // public void setInstructions(String instructions) {
+    //     this.instructions = instructions;
+    // }
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
+    // public Date getCreatedAt() {
+    //     return createdAt;
+    // }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+    // public void setCreatedAt(Date createdAt) {
+    //     this.createdAt = createdAt;
+    // }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
+    // public Date getUpdatedAt() {
+    //     return updatedAt;
+    // }
 
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+    // public void setUpdatedAt(Date updatedAt) {
+    //     this.updatedAt = updatedAt;
+    // }
 
-    public User getUser() {
-        return user;
-    }
+    // public User getUser() {
+    //     return user;
+    // }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+    // public void setUser(User user) {
+    //     this.user = user;
+    // }
 }
